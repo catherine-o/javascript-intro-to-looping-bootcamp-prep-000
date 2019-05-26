baf7f7b38cff5fa6af0817bc5a95c870214de76a
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-26 22:04:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,7 +16,6 @@ function whileLoop(n) {
 }
 
 
-var num = 0;
 
 function doWhileLoop(num) {
   num = num + 1;
@@ -25,4 +24,4 @@ function doWhileLoop(num) {
 
 do {
   console.log("I run once regardless.");
-} while (doWhileLoop() < num); 
+} while (doWhileLoop() < num); s
